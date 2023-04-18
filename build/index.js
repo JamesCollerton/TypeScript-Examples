a0b01f962fc1ed10aa829dee33042ef484b0d438
James Collerton <jamescollerton@live.co.uk>
2023-04-18 17:20:23 +0100

    Done all TS examples

--- build/index.js
+++ build/index.js
@@ -32,3 +32,94 @@ console.log(convertCoordinatesToString({ x: 1, y: 2, z: 3 }));
 const coords = { x: 5, y: 6 };
 // Prints 5.6
 console.log(convertCoordinatesToString(coords));
+// We can assign one to the other, seeing as they
+// have the same structure
+const variableOne = { a: "A" };
+const variableTwo = variableOne;
+// Same function as previously using the type
+function convertTypeToString(t) {
+    return `A value: ${t.a}`;
+}
+console.log(convertTypeToString({ a: "A" }));
+// Here car expects all three fields
+const car = { name: "Car", size: "M", numberWheels: 4 };
+// Vehicle can needs only two
+const vehicle = { name: "Plane", size: "L" };
+// This is the syntax for limiting our size variable to only
+// one of three values
+const size = 'M';
+// Here we have a function that only accepts
+// our S, M, L arguments
+function printSize(size) {
+    console.log(size);
+}
+// Here we create a string with a valid value
+const sizeString = 'M';
+// Now we try and print the string. Unless we
+// assert it is of the correct type the compiler
+// will recognise it as a string and throw an error!
+printSize(sizeString);
+// Here we use the type and declare a function which takes
+// in a string and prints it
+const exampleFn = a => console.log(a);
+// Now we pass to something expecting an argument which is a
+// function taking in a string and returning void. It then executes
+// the function with the string 'Hello everyone!'
+function executeFunction(fn) {
+    fn("Hello everyone!");
+}
+// This prints 'Hello everyone!'. Notice how the structure of the
+// ExampleFnType is the same as the method signature, and so this
+// is a valid call
+executeFunction(exampleFn);
+// Here we define a function which can (or can not) take 
+// a parameter
+function functionOptionalParam(x) {
+    if (x) {
+        console.log("x is defined");
+    }
+    else {
+        console.log("x is not defined");
+    }
+}
+// Prints 'x is not defined'
+functionOptionalParam();
+// Prints 'x is defined'
+functionOptionalParam(7);
+// Here we define a function which can (or can not) take 
+// a parameter, but if one isn't supplied we'll use 10
+function functionDefaultParam(x = 10) {
+    console.log("x + 10 is " + (x + 10));
+}
+// Prints 'x + 10 is 20'
+functionDefaultParam();
+// Prints 'x + 10 is 17'
+functionDefaultParam(7);
+// Now we take in an object of type Coords, but
+// use the destructuring syntax to be able to
+// directly reference its parts
+function sum({ x, y }) {
+    console.log("Sum of coords is " + (x + y));
+}
+// Prints 'Sum of coords is 3'
+sum({ x: 1, y: 2 });
+// We can initialise a variable of the
+// type here
+const readOnly = {
+    unchangeable: "a",
+    changeable: { key: "value one" }
+};
+// This wouldn't work as it's read only
+// readOnly.unchangeable = "b"
+// However, this is fine as we're not reassigning
+// the type itself
+readOnly.changeable.key = "value two";
+// Here we can receive a string or number, then
+// depending on the type print a different result
+function typeGuardFunction(x) {
+    typeof x === "string" ? console.log("x is a string!") : console.log("x is a number!");
+}
+// Prints 'x is a string!'
+typeGuardFunction("1");
+// Prints 'x is a number!'
+typeGuardFunction(1);
--- src/index.ts
+++ src/index.ts
@@ -51,3 +51,179 @@ const coords: Coordinates = {x: 5, y: 6}
 
 // Prints 5.6
 console.log(convertCoordinatesToString(coords))
+
+// Here we have two structurally equivalent types
+type TypeOne = {
+  a: string;
+}
+type TypeTwo = {
+  a: string;
+}
+
+// We can assign one to the other, seeing as they
+// have the same structure
+const variableOne: TypeOne = {a: "A"};
+const variableTwo: TypeTwo = variableOne; 
+
+type TypeThree = {
+  a: string;
+  b: string;
+}
+
+// Same function as previously using the type
+function convertTypeToString(t: TypeOne) {
+  return `A value: ${t.a}`
+}
+
+console.log(convertTypeToString({a: "A"}))
+
+// Here we declare our first vehicle interface.
+interface Vehicle {
+  name: string 
+}
+
+// Now we add a size field to the interface, keeping
+// the existing ones
+interface Vehicle {
+  size: string
+}
+
+// Now we create a new interface with all of the old
+// fields, but an extra one on top.
+interface Car extends Vehicle {
+  numberWheels: number
+}
+
+// Here car expects all three fields
+const car: Car = {name: "Car", size: "M", numberWheels: 4}
+
+// Vehicle can needs only two
+const vehicle: Vehicle = {name: "Plane", size: "L"}
+
+// This is the syntax for limiting our size variable to only
+// one of three values
+const size: 'S' | 'M' | 'L' = 'M'
+
+// Here we have a function that only accepts
+// our S, M, L arguments
+function printSize(size: 'S' | 'M' | 'L') {
+  console.log(size)
+}
+
+// Here we create a string with a valid value
+const sizeString: string = 'M'
+
+// Now we try and print the string. Unless we
+// assert it is of the correct type the compiler
+// will recognise it as a string and throw an error!
+printSize(sizeString as 'M')
+
+// Here we declare a type which maps to a function taking
+// in a string and returning nothing
+type ExampleFnType = (a: string) => void
+
+// Here we use the type and declare a function which takes
+// in a string and prints it
+const exampleFn: ExampleFnType = a => console.log(a)
+
+// Now we pass to something expecting an argument which is a
+// function taking in a string and returning void. It then executes
+// the function with the string 'Hello everyone!'
+function executeFunction(fn: (a: string) => void) {
+  fn("Hello everyone!");
+}
+
+// This prints 'Hello everyone!'. Notice how the structure of the
+// ExampleFnType is the same as the method signature, and so this
+// is a valid call
+executeFunction(exampleFn)
+
+// Here we define a function which can (or can not) take 
+// a parameter
+function functionOptionalParam(x?: number) {
+  if(x) {
+    console.log("x is defined")  
+  } else {
+    console.log("x is not defined")  
+  }
+}
+
+// Prints 'x is not defined'
+functionOptionalParam()
+
+// Prints 'x is defined'
+functionOptionalParam(7)
+
+// Here we define a function which can (or can not) take 
+// a parameter, but if one isn't supplied we'll use 10
+function functionDefaultParam(x = 10) {
+  console.log("x + 10 is " + (x + 10))
+}
+
+// Prints 'x + 10 is 20'
+functionDefaultParam()
+
+// Prints 'x + 10 is 17'
+functionDefaultParam(7)
+
+// Here we define a type
+type Coords = { x: number; y: number; };
+
+// Now we take in an object of type Coords, but
+// use the destructuring syntax to be able to
+// directly reference its parts
+function sum({ x, y }: Coords) {
+  console.log("Sum of coords is " + (x + y));
+}
+
+// Prints 'Sum of coords is 3'
+sum({x: 1,y: 2})
+
+// Creating a type with two read only fields. One
+// is a string, the other an object with a single
+// value
+type ReadOnlyType = {
+  readonly unchangeable: string,
+  readonly changeable: {key: string}
+}
+
+// We can initialise a variable of the
+// type here
+const readOnly: ReadOnlyType = {
+  unchangeable: "a",
+  changeable: {key: "value one"}
+}
+
+// This wouldn't work as it's read only
+// readOnly.unchangeable = "b"
+
+// However, this is fine as we're not reassigning
+// the type itself
+readOnly.changeable.key = "value two"
+
+// Create one type
+type NameType = {
+  name: string
+}
+
+// Create another type
+type AgeType = {
+  age: number
+}
+
+// Combine them! Note, we're not covering
+// the case where both types had the same
+// field!
+type NameAndAgeType = NameType & AgeType
+
+// Here we can receive a string or number, then
+// depending on the type print a different result
+function typeGuardFunction(x: string | number) {
+  typeof x === "string" ? console.log("x is a string!") : console.log("x is a number!")
+}
+
+// Prints 'x is a string!'
+typeGuardFunction("1")
+
+// Prints 'x is a number!'
+typeGuardFunction(1)
